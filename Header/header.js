26402054b2be0773793f273c39fb8eb6f38d9b6f
Alejo <aebmbu@gmail.com>
2021-10-07 12:06:31 -0300

    commit hasta la welcome

---
+++ Header/header.js
@@ -0,0 +1,39 @@
+
+function headerComponent (el){
+    const componentEl = document.createElement("div")
+
+    componentEl.innerHTML = 
+    `   <header class="header">
+        <img class="header__logo" src="Logo Alejo.png" alt="Logo">
+        <label for="Menu" class="contenedor-menu">
+            <div id="Menu" class="menu"></div>
+            <div id="Menu" class="menu"></div>
+            <div id="Menu" class="menu"></div>
+        </label>
+            <div class="menu_display">
+                <div class="contenedor_boton">
+                    <button class="boton_cerrar-menu">X</button>
+                </div>
+                <div class="contenedor_titulo">
+                    <div class="menu__title">Portfolio</div>
+                    <div class="menu__title">Servicios</div>
+                    <div class="menu__title">Contacto</div>
+                </div>
+            </div>
+            </header>
+    `
+;
+const AbrirMenuEl = componentEl.querySelector(".contenedor-menu");
+const CerrarMenuEl = componentEl.querySelector(".boton_cerrar-menu");
+const VentanaMenuEl = componentEl.querySelector(".menu_display");
+
+AbrirMenuEl.addEventListener("click",()=>{
+    VentanaMenuEl.style.display = "inherit"
+} )
+CerrarMenuEl.addEventListener("click",()=>{
+    VentanaMenuEl.style.display = ""
+} )
+
+    el.appendChild(componentEl)
+}    
+
---
+++ index.js
@@ -0,0 +1,123 @@
+function addWelcome (params = {}){
+    const template = document.querySelector("#welcome-template");
+    const container = document.querySelector(".welcome");
+
+    template.content.querySelector(".title").textContent = params.titulo
+    template.content.querySelector(".subtitle").textContent = params.subtitle 
+
+    const clone = document.importNode(template.content, true);
+    container.appendChild(clone);
+}
+
+function getWelcome (){
+    return fetch("https://cdn.contentful.com/spaces/d58j3u5qs7mw/environments/master/entries?access_token=2e-JDwddkCox7wsqfgnbT5YTpSwQ938Bf2gouDJVGKk&content_type=bienvenida")
+    .then(res =>{
+    return res.json()
+    }).then((data)=>{
+      //console.log(data);
+      const item = data.items["0"].fields;
+      const params = {
+        titulo: item.titulo,
+        subtitle: item.subtitle,
+        }
+        addWelcome(params)
+    })
+    }
+
+    
+     
+
+ 
+function addIntro (params = {}){
+    const template = document.querySelector("#presentacion-template");
+    const container = document.querySelector(".presentacion-content");
+
+    template.content.querySelector(".presentacion__title").textContent = params.titulo;
+    template.content.querySelector(".presentacion__text").textContent = params.textopresentacion;
+    template.content.querySelector(".presentacion__img").src = params.foto;
+
+    const clone = document.importNode(template.content, true);
+    container.appendChild(clone);
+}
+
+
+function getIntro (){
+return fetch("https://cdn.contentful.com/spaces/d58j3u5qs7mw/environments/master/entries?access_token=2e-JDwddkCox7wsqfgnbT5YTpSwQ938Bf2gouDJVGKk&content_type=presentacion")
+.then(res =>{
+return res.json()
+}).then((data)=>{
+    const item = data.items["0"].fields
+    const imgURL = data.includes.Asset["0"].fields.file.url;
+
+    const params = {
+        titulo: item.titulo,
+        textopresentacion: item.textopresentacion,
+        foto: imgURL
+    };
+    addIntro(params);
+});
+}
+
+function addServices (params = {}){
+
+    const template = document.querySelector("#servicios__template");
+    const container = document.querySelector(".servicios__content");
+    
+    template.content.querySelector(".service__img").src = params.imagen;
+
+    template.content.querySelector(".service__title").textContent = params.subtitulo;
+
+    template.content.querySelector(".service__text").textContent = params.parrafo;
+
+    const clone = document.importNode(template.content, true);
+    container.appendChild(clone);
+}
+
+function getServices (){
+return fetch("https://cdn.contentful.com/spaces/d58j3u5qs7mw/environments/master/entries?access_token=2e-JDwddkCox7wsqfgnbT5YTpSwQ938Bf2gouDJVGKk&content_type=servicios")
+.then(res =>{
+return res.json()
+}).then((data)=>{
+    const fieldsCollection = data.items.map((item) => {
+        return {
+          subtitulo: item.fields.subtitulo,
+          parrafo: item.fields.parrafo,
+          imageID: item.fields.imagen.sys.id,
+          includes: data.includes.Asset,
+        };
+      });
+      fieldsCollection.forEach((item) => {
+        const id = buscarAsset(item.imageID, item.includes);
+        item.imagen = "https:" + id.fields.file.url;
+      });
+      return fieldsCollection;
+    });
+}
+
+function buscarAsset(imageID, includes) {
+  const found = includes.find((res) => {
+    return res.sys.id == imageID;
+  });
+  return found;
+}
+
+function main (){
+const headerEl = document.querySelector(".header-container");
+headerComponent(headerEl)
+
+getWelcome()
+getIntro()
+
+getServices().then(function(data) {
+    for (const r of data) {
+        addServices(r)
+    }
+})
+}
+
+
+main()
+
+
+
+
