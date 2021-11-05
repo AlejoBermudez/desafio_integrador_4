5257e2d1481184b5291c41747f021ae4e330dfb2
Alejo <aebmbu@gmail.com>
2021-11-05 15:42:42 -0300

    links de imgs

--- Footer/footer.js
+++ Footer/footer.js
@@ -3,14 +3,15 @@ function footerComponent (el){
 
     footerEl.innerHTML = 
     `<footer class="footer">
-    <img src="/imagenes/Logo Alejo.png" class="footer_logo" alt="Logo Alejo">
+    <img src="https://github.com/AlejoBermudez/desafio_integrador_4/blob/main/imagenes/Logo%20Alejo.png?raw=true
+    " class="footer_logo" alt="Logo Alejo">
     <div class="redes">
         <p class="footer__redes">Instagram</p>
-        <img src="/imagenes/instagram.png" class="footer__img" alt="">
+        <img src="https://github.com/AlejoBermudez/desafio_integrador_4/blob/main/imagenes/instagram.png?raw=true" class="footer__img" alt="">
         <p class="footer__redes">Linkdn</p>
-        <img src="/imagenes/linkedin.png" class="footer__img" alt="">
+        <img src="https://github.com/AlejoBermudez/desafio_integrador_4/blob/main/imagenes/linkedin.png?raw=true" class="footer__img" alt="">
         <p class="footer__redes">Youtube</p>
-        <img src="/imagenes/youtube.png" class="footer__img" alt="">
+        <img src="https://github.com/AlejoBermudez/desafio_integrador_4/blob/main/imagenes/youtube.png?raw=true" class="footer__img" alt="">
     </div>
 </footer>
     `
